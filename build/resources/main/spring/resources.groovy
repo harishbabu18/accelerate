3face203f1495c3b4f853f4025766334fb140252
harishbabusdc <harish.babu@sdc.company>
2020-09-27 12:45:14 +0530

    Removed Unneccesary Folder Commits

--- build/resources/main/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-import usermanagement.CustomAppRestAuthTokenJsonRenderer
-import usermanagement.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener)
-    accessTokenJsonRenderer(CustomAppRestAuthTokenJsonRenderer)
-
-}
